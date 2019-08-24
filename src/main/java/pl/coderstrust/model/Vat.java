6989c7df72c91ce980d8d7244bf8579cd5ed02ca
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-08-24 13:19:42 +0200

    added values for VAT enums added constructor, getValue and toString methods for Vat enum

--- src/main/java/pl/coderstrust/model/Vat.java
+++ src/main/java/pl/coderstrust/model/Vat.java
@@ -1,9 +1,28 @@
 package pl.coderstrust.model;
 
+import java.math.BigDecimal;
+
 public enum Vat {
 
-    VAT_0,
-    VAT_5,
-    VAT_8,
-    VAT_23
+    VAT_0(new BigDecimal("0.00")),
+    VAT_5(new BigDecimal("0.05")),
+    VAT_8(new BigDecimal("0.08")),
+    VAT_23(new BigDecimal("0.23"));
+
+    private final BigDecimal value;
+
+    Vat(BigDecimal value) {
+        this.value = value;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Vat{"
+                + "value=" + value
+                + '}';
+    }
 }
