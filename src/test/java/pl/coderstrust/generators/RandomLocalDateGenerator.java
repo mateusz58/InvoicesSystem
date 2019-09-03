6369e05154c370036aa15a5f70184cf008003a6c
mateusz58 <matp321@gmail.com>
2019-09-03 15:33:46 +0200

    Update src/test/java/pl/coderstrust/generators/RandomLocalDateGenerator.java
    Co-Authored-By: Paweł Zięba <zieba.pawel@gmail.com>

--- src/test/java/pl/coderstrust/generators/RandomLocalDateGenerator.java
+++ src/test/java/pl/coderstrust/generators/RandomLocalDateGenerator.java
@@ -3,7 +3,7 @@ package pl.coderstrust.generators;
 import java.time.LocalDate;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class RandomLocalDateGenerator {
+public class LocalDateGenerator {
 
     public static LocalDate generateRandomLocalDate() {
         int year = ThreadLocalRandom.current().nextInt(1976, 2019);
