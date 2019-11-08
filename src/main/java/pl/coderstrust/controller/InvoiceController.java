0c3846317e75aa712fed4e26458737461de88492
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-11-08 22:11:58 +0100

    wip2

--- src/main/java/pl/coderstrust/controller/InvoiceController.java
+++ src/main/java/pl/coderstrust/controller/InvoiceController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,6 @@ import pl.coderstrust.service.ServiceOperationException;
 
 @RestController
 @RequestMapping("/invoices")
-@CrossOrigin
 @Api(value = "/invoices")
 public class InvoiceController {
 
