d7fe088da40bb92545e664d3eb21ec80482e52b1
zlp <matp321@gmail.com>
2019-11-15 20:57:44 +0100

    Add  Applied rebase 2

--- src/main/java/pl/coderstrust/configuration/SqlConfiguration.java
+++ src/main/java/pl/coderstrust/configuration/SqlConfiguration.java
@@ -2,8 +2,10 @@ package pl.coderstrust.configuration;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:postgresql.properties")
 @ConditionalOnProperty(name = "pl.coderstrust.database", havingValue = "postgresql")
 public class SqlConfiguration {
 }
