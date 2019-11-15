413b0271efc8b40005a216ee0cf4458b44d34373
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-11-15 22:24:57 +0100

    pdf button

--- src/main/resources/public/invoices.js
+++ src/main/resources/public/invoices.js
@@ -1,7 +1,15 @@
 angular.module('invoices', [])
-.controller('Invoices', function($scope, $http) {
-    $http.get('http://localhost:8080/invoices').
+.controller('Invoices', function($scope, $http, $window) {
+    $http.get(getBaseApiAddress()).
         then(function(response) {
             $scope.invoices = response.data;
         });
+
+    $scope.pdf = function(id) {
+        $window.open(getBaseApiAddress() + 'pdf/' + id);
+    }
 });
+
+function getBaseApiAddress() {
+    return 'http://localhost:8080/invoices/';
+}
