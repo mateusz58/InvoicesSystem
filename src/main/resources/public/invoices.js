d4e1ee1286ad7eff26a989e3fabe14540920777b
Jakub Kasprzak <kasprzakjak@poczta.onet.pl>
2019-11-17 14:44:26 +0100

    delete button works main site is now reloaded after deleting invoice

--- src/main/resources/public/invoices.js
+++ src/main/resources/public/invoices.js
@@ -8,7 +8,24 @@ angular.module('invoices', [])
     $scope.pdf = function(id) {
         $window.open(getBaseApiAddress() + 'pdf/' + id);
     }
+
+    $scope.delete = function(id) {
+        $http.delete(getBaseApiAddress() + id).
+            then(function() {
+                $http.get(getBaseApiAddress()).
+                    then(function(response) {
+                        $scope.invoices = response.data;
+                    });
+            });
+    }
 });
+
+function getMainSite() {
+    $http.get(getBaseApiAddress()).
+    then(function(response) {
+        $scope.invoices = response.data;
+    });
+}
 
 function getBaseApiAddress() {
     return 'http://localhost:8080/invoices/';
