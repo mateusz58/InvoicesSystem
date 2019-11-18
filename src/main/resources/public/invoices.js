315918665898b4ba677a10fc4f69059f1d9ae064
A5Kc2 <kasprzakjak@poczta.onet.pl>
2019-11-18 10:42:23 +0100

    review applied

--- src/main/resources/public/invoices.js
+++ src/main/resources/public/invoices.js
@@ -1,31 +1,23 @@
 angular.module('invoices', [])
 .controller('Invoices', function($scope, $http, $window) {
-    $http.get(getBaseApiAddress()).
-        then(function(response) {
-            $scope.invoices = response.data;
-        });
+    getAllInvoices();
 
     $scope.pdf = function(id) {
         $window.open(getBaseApiAddress() + 'pdf/' + id);
     }
 
     $scope.delete = function(id) {
-        $http.delete(getBaseApiAddress() + id).
-            then(function() {
-                $http.get(getBaseApiAddress()).
-                    then(function(response) {
-                        $scope.invoices = response.data;
-                    });
-            });
+        $http.delete(getBaseApiAddress() + id).then(function() {
+            getAllInvoices();
+        });
+    }
+
+    function getAllInvoices() {
+        $http.get(getBaseApiAddress()).then(function(response) {
+            $scope.invoices = response.data;
+        });
     }
 });
-
-function getMainSite() {
-    $http.get(getBaseApiAddress()).
-    then(function(response) {
-        $scope.invoices = response.data;
-    });
-}
 
 function getBaseApiAddress() {
     return 'http://localhost:8080/invoices/';
