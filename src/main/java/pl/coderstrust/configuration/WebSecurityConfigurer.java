e6a66b1b4023e28c2d1c7aaee6b341b51dbc24c4
zlp <matp321@gmail.com>
2019-12-04 16:01:44 +0100

    Deleted hibernate configurer

--- src/main/java/pl/coderstrust/configuration/WebSecurityConfigurer.java
+++ src/main/java/pl/coderstrust/configuration/WebSecurityConfigurer.java
@@ -1,17 +1,13 @@
 package pl.coderstrust.configuration;
 
-import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
