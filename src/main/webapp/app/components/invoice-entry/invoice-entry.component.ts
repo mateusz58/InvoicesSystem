65d57b78f1948e24115e9af5d2df4963380d4470
mateusz58 <matp321@gmail.com>
2020-02-20 04:26:27 +0100

    added models and components

---
+++ src/main/webapp/app/components/invoice-entry/invoice-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-entry',
+  templateUrl: './invoice-entry.component.html',
+  styles: []
+})
+export class InvoiceEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
